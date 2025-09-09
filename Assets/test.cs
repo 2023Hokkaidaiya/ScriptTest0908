4b8767757d10f8e5b39d4ff4604af5dea2a0837e
2023Hokkaidaiya <122017629+2023Hokkaidaiya@users.noreply.github.com>
2025-09-09 19:44:16 +0900

    Update test.cs

--- Assets/test.cs
+++ Assets/test.cs
@@ -11,12 +11,12 @@ public class Test : MonoBehaviour
         //要素の個数が5の、int型の配列arrayを宣言して好きな値で初期化
         int[] array = new int[] { 1, 3, 5, 7, 31 };
         //for文を使い、配列の各要素の値を順番に表示
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < array.Length; i++)
         {
             Debug.Log(array[i]);
         }
         //for文を使い、配列の各要素の値を逆順に表示
-        for (int i = 4; i > -1; i--)
+        for (int i = array.Length - 1; i >= 0; i--)
         {
             Debug.Log(array[i]);
         }
